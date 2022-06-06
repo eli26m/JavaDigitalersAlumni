31ea0d5b97e295b9e895d30b84e0821a38b8b0ee
eli26m <veintiseisdemarzo@gmail.com>
2022-06-06 19:07:35 -0300

    arreglado ejercicio LaboratorioUnidad3_IfElse_Ejercicio1

--- Unidad3/src/LaboratorioUnidad3_IfElse_Ejercicio1.java
+++ Unidad3/src/LaboratorioUnidad3_IfElse_Ejercicio1.java
@@ -8,15 +8,15 @@ public class LaboratorioUnidad3_IfElse_Ejercicio1 {
 		
 		System.out.println("Ingrese usuario:");
 		String usuario = entrada.nextLine();
-		System.out.println("Ingrese contraseña:");
-		String contraseña = entrada.nextLine();
+		System.out.println("Ingrese clave:");
+		String clave = entrada.nextLine();
 		
 		entrada.close();
 		
 		String usuarioCorrecto = "persona";
-		String contraseñaCorrecta = "1234";
+		String claveCorrecta = "1234";
 		
-		if ((usuario.equals(usuarioCorrecto)) && (contraseña.equals(contraseñaCorrecta))) {
+		if ((usuario.equals(usuarioCorrecto)) && (clave.equals(claveCorrecta))) {
 			System.out.println("\nBIENVENIDO AL SISTEMA");
 		} else {
 			System.out.println("\nCREDENCIALES INCORRECTAS");
